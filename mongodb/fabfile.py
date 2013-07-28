0f575d21943ba2aa0f72dcd50b6bc9a9fd4c4872
Lei Xu <eddyxu@gmail.com>
2013-07-28 15:06:01 -0500

    * cleanup mongodb start script

--- mongodb/fabfile.py
+++ mongodb/fabfile.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 (c) Lei Xu <eddyxu@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Install and start/stop MongoDB.
 """
 
@@ -5,8 +21,9 @@ from __future__ import print_function, division
 from fabric.api import task, env, execute, roles, run, local, settings
 from fabric.api import parallel
 import os
-import socket
 import pymongo
+import shutil
+import socket
 import sys
 sys.path.append('..')
 from fablib import base_dir, download_tarball, run_background
@@ -43,7 +60,8 @@ def start_config_server():
 
 @parallel
 def start_shared_server():
-    run_background('%(mongo_bin)s --shardsvr --port 27017 --dbpath %(data_dir)s' % env)
+    run_background('%(mongo_bin)s --shardsvr --port 27017 '
+                   '--dbpath %(data_dir)s' % env)
 
 
 @roles('head')
@@ -72,13 +90,16 @@ def start(num_shard):
     num_shard = int(num_shard)
     download_tarball(URL)
 
-    if not os.path.exists(env.data_dir):
-        os.makedirs(env.data_dir)
+    if os.path.exists(env.config_dir):
+        shutil.rmtree(env.config_dir)
+
+    os.makedirs(env.data_dir)
 
     execute(start_config_server)
     print(env.workers)
     execute(start_shared_server, hosts=env.workers[:num_shard])
 
+    local('sleep 2')
     conn = pymongo.Connection(env.head)
     admin = conn.admin
     for shard in env.workers[:num_shard]:
